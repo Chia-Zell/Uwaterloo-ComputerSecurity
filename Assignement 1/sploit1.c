9865d758c9ee8fc3c56dd2a846cbe2218c77e71b
Zellccc <gory-killer@hotmail.com>
2018-01-11 06:46:52 +0800

    update buff overflow

--- Assignement 1/sploit1.c
+++ Assignement 1/sploit1.c
@@ -16,15 +16,9 @@ int main(void)
 
   // one way to invoke pwgen, system() creates a separate process
   char *buf = 'x00' * 1000 +'\0';
-  system("/usr/local/bin/pwgen -v");
+  system("/usr/local/bin/pwgen -v" + buf);
 
 
-  env[0] = NULL;
-  // execve() executes the target program by overwriting the
-  // memory of the process in which execve() is executing, i.e.,
-  // execve() should never return
-  if (execve(TARGET, args, env) < 0)
-    fprintf(stderr, "execve failed.\n");
 
   exit(0);
 }
