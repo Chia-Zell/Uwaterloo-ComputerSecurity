62fcaeb42db9bd7cf71e3cae9bc1078c17c25582
ccc <last_remedy@outlook.com>
2018-03-06 00:05:30 -0500

    add tars

---
+++ Assignment2/tarall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+
+tar cvf exploit1_a.tar -C exploit1_a .
+tar cvf exploit1_b.tar -C exploit1_b .
+tar cvf exploit1_c.tar -C exploit1_c .
+tar cvf exploit2.tar -C exploit2 .
+tar cvf exploit3.tar -C exploit3 .
+
